56d552eda58a86fe5529d44aaf8d046f1520dd0f
rafaelnargolo <124110743+rafaelnargolo@users.noreply.github.com>
2025-04-24 23:52:49 -0300

    Atualzando

---
+++ src/roteiro6/parte3/SQLServer.java
@@ -0,0 +1,10 @@
+package roteiro6.parte3;
+
+public class SQLServer implements Connection {
+
+    @Override
+    public void connect() {
+        System.out.println("Conectando ao SQL Server");
+    }
+
+}
